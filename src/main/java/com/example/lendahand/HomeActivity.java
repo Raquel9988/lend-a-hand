e32dc981606f186d16f22cd2a161336e127b4f61
StheshSphere <stheshsphere@gmail.com>
2025-05-26 18:19:45 +0200

    Update HomeActivity.java

--- src/main/java/com/example/lendahand/HomeActivity.java
+++ src/main/java/com/example/lendahand/HomeActivity.java
@@ -4,11 +4,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
 
 public class HomeActivity extends AppCompatActivity {
 
